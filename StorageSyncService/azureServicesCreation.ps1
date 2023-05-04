cc45f4354bd3a8b9eb7a700eec9aab2586c319a8
sasidharanshanmugam <sasichithra98@gmail.com>
2023-05-04 17:11:14 +0530

    mmm

--- StorageSyncService/azureServicesCreation.ps1
+++ StorageSyncService/azureServicesCreation.ps1
@@ -3,6 +3,14 @@ az login --service-principal -u 8bee5ca0-27f1-4e77-b60a-de34ae42eb32 -p tXO8Q~at
 
  
 # az deployment group create --resource-group MSFT-assessment --template-file deploy.json --parameters parameters.json enableServerEndpoint=true serverID='920f2ffa-9283-438d-9a38-e61a1d7070ae'
-az deployment group create --resource-group MSFT-assessment --template-file deploy.json --parameters parameters.json enableServerEndpoint=true serverID='920f2ffa-9283-438d-9a38-e61a1d7070ae'
+# az deployment group create --resource-group MSFT-assessment --template-file deploy.json --parameters parameters.json enableServerEndpoint=true serverID='920f2ffa-9283-438d-9a38-e61a1d7070ae'
+# az group create --name templateSpecRG --location westus2
 
+# az ts create  --name webSpec --version "1.0.0.0" --resource-group MSFT-assessment --location "westus2" --template-file "C:\Users\User\Desktop\poc\storageaccount\deploy.json"
 
+# az group create --name MSFT-assessment --location westus2
+# id = $(az ts show --name webSpec --resource-group MSFT-assessment --version "1.0.0.0" --query "id")
+# az deployment group create --resource-group MSFT-assessment --template-spec "/subscriptions/732486d5-9184-4d49-a508-bb533cdcd6b6/resourceGroups/MSFT-assessment/providers/Microsoft.Resources/templateSpecs/webSpec/versions/1.0.0.0" 
+az deployment group create --resource-group MSFT-assessment --template-file deploy.json --parameters parameters.json
+# New-AzResourceGroupDeployment -ResourceGroupName MSFT-assessment -TemplateFile deploy.json -TemplateParameterFile parameters.json 
+# -deploymentFilePath storageaccount\deploy.json
