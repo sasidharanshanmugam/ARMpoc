f8d9dac3a3af1ad0e08636a3142623d3572a864d
sasidharanshanmugam <sasichithra98@gmail.com>
2023-05-05 14:27:22 +0530

    codechanges

--- StorageSyncService/azureServicesCreation.ps1
+++ StorageSyncService/azureServicesCreation.ps1
@@ -11,6 +11,7 @@ az login --service-principal -u 8bee5ca0-27f1-4e77-b60a-de34ae42eb32 -p tXO8Q~at
 # az group create --name MSFT-assessment --location westus2
 # id = $(az ts show --name webSpec --resource-group MSFT-assessment --version "1.0.0.0" --query "id")
 # az deployment group create --resource-group MSFT-assessment --template-spec "/subscriptions/732486d5-9184-4d49-a508-bb533cdcd6b6/resourceGroups/MSFT-assessment/providers/Microsoft.Resources/templateSpecs/webSpec/versions/1.0.0.0" 
-az deployment group create --resource-group MSFT-assessment --template-file deploy.json --parameters parameters.json
+az deployment group create --resource-group MSFT-assessment --template-file deploycopy.json --parameters parameters.json 
+# --template-uri "https://raw.github.com/sasidharanshanmugam/ARMpoc/tree/master/storageaccount"
 # New-AzResourceGroupDeployment -ResourceGroupName MSFT-assessment -TemplateFile deploy.json -TemplateParameterFile parameters.json 
 # -deploymentFilePath storageaccount\deploy.json
